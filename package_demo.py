da0b2542578b631c056a56432b558f6e8e616463
keerthanatangallapally <keerthanatangallapally8@gmail.com>
2026-07-19 16:16:15 +0530

    Day 3: Learned Python packages and practiced imports

---
+++ package_demo.py
@@ -0,0 +1,18 @@
+import math
+from datetime import time, date, datetime
+
+print(math.sin(2))
+
+print(math.cos(2))
+
+print(math.tan(2))
+
+a = (datetime(2026, 7, 19, 14,10))
+print(a.strftime('%B  %m  %y'))
+
+print(datetime.now())
+
+print(time(14,12))
+
+print(date.today())
+
